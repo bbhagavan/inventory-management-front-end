da58ef3b984e78953bf53e753cbfdbf529493331
Bhagavan Bejjipurapu <bhagavan.bejjipurapu@acsicorp.com>
2021-04-27 22:07:01 +0530

    Adding backEnd

---
+++ back-end-java-rest-api/components/Home.js
@@ -0,0 +1,10 @@
+import { Jumbotron } from "react-bootstrap";
+
+export default function Home() {
+    return (
+        <Jumbotron>
+            <h2>Welcome to Brand Shoppig</h2>
+            <p>Enjoy best range of products</p>
+        </Jumbotron>
+    );
+}
